add7f2ea2df6ff1ad3a26a6764692c3374f7974c
HassanNasser <hassanmsr03@gmail.com>
2025-11-29 22:32:58 +0200

    updata sebha tap

--- lib/taps/sebha_tap.dart
+++ lib/taps/sebha_tap.dart
@@ -18,59 +18,70 @@ class _SebhaTapState extends State<SebhaTap> {
 
   int askarcount = 0;
   int index = 0;
+  double angle = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'سَبِّحِ اسْمَ رَبِّكَ الأعلى ',
-          style: Theme.of(context).textTheme.displaySmall,
-        ),
-        InkWell(
-          onTap: () {
-            askarcount++;
-            if (askarcount == 33) {
-              index++;
-              askarcount = 0;
-            }
-            if (index > 3) {
-              index = 0;
-            }
-            setState(() {});
-          },
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 26),
-            child: Container(
-              alignment: Alignment.center,
-              height: 460,
-              // margin: const EdgeInsets.symmetric(horizontal: 26),
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/Sebha.png'),
-                  // fit: BoxFit.fill,
+    var screenSize = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: screenSize.height * 0.02),
+          Text(
+            'سَبِّحِ اسْمَ رَبِّكَ الأعلى ',
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+          SizedBox(height: screenSize.height * 0.02),
+          Image.asset('assets/images/SebhaTail.png', height: 85, width: 145),
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              InkWell(
+                onTap: onClicked,
+                child: Transform.rotate(
+                  angle: angle,
+                  child: Image.asset('assets/images/SebhaBody.png'),
                 ),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(height: 50),
-                  Text(
-                    '${azkar[index]}',
-                    style: Theme.of(context).textTheme.displaySmall,
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    '$askarcount',
-                    style: Theme.of(context).textTheme.displaySmall,
-                  ),
-                ],
+
+              SizedBox(
+                width: screenSize.width * 0.6,
+                child: Column(
+                  children: [
+                    Text(
+                      ' ${azkar[index]}',
+                      style: Theme.of(context).textTheme.displaySmall,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      '$askarcount',
+                      style: Theme.of(context).textTheme.displaySmall,
+                    ),
+                  ],
+                ),
               ),
-            ),
+            ],
           ),
-        ),
-      ],
+
+         
+        ],
+      ),
     );
+  }
+
+  void onClicked() {
+    askarcount++;
+    angle += 20;
+    if (askarcount % 33 == 0) {
+      index++;
+    }
+    if (index == azkar.length) {
+      index = 0;
+      askarcount = 0;
+    }
+    setState(() {});
   }
 }
